7e16e23c992ce8ec83254a93623a36a945099d59
Kevin-Zagar <43926874+Kevin-Zagar@users.noreply.github.com>
2019-10-18 08:13:35 +0200

    Changed CSS File

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo-Liste</h1>
+      <ul className="App-todo">
+        <li>Obst kaufen</li>
+        <li>Programmieren</li>
+        <li>3. TODO</li>
+
+      </ul>
     </div>
   );
 }
