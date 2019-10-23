500adc2f7249a35fb95dba622b6504b2d963eb74
Kevin-Zagar <43926874+Kevin-Zagar@users.noreply.github.com>
2019-10-23 09:54:08 +0200

    Installed styled comopnents

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Todo-Liste</h1>
-      <ul className="App-todo">
-        <li>Obst kaufen</li>
-        <li>Programmieren</li>
-        <li>3. TODO</li>
+class App extends Component {
 
-      </ul>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos: ["Obst kaufen", "Programieren lernen", "Lesen"]
+        }
+        this.addTodo = this.addTodo.bind(this);
+    }
+
+    addTodo(){
+        let todos = this.state.todos;
+        todos.push("4. Todo");
+        this.setState({
+            todos: todos
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                
+                <h1>Todo-Liste</h1>
+                <ul className="App-todo">
+                    {this
+                        .state
+                        .todos
+                        .map((todo) => {
+                            return (
+                                <li>{todo}</li>
+                            )
+                        })}
+                </ul>
+                <button onClick={this.addTodo}>Todo hinzufügen</button>
+            </div>
+        );
+    }
 }
 
 export default App;
